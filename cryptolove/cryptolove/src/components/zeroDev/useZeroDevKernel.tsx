b0044682f44b00e4f819d38be347da4de4296545
DAppaDan <93945794+DappaDanDev@users.noreply.github.com>
2024-02-13 16:43:08 +0100

    setting up the zerodev scaffold

---
+++ cryptolove/cryptolove/src/components/zeroDev/useZeroDevKernel.tsx
@@ -0,0 +1,40 @@
+// Just as Magic handles the creation of user wallets, ZeroDev handles the creation of smart contract accounts associated with the wallet.
+
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useMagic } from "../magic/MagicProvider";
+
+import { createEcdsaKernelAccountClient } from "@zerodev/presets/zerodev";
+import { providerToSmartAccountSigner } from "permissionless";
+import { polygonMumbai } from "viem/chains";
+
+export const useZeroDevKernel = () => {
+	const { magic } = useMagic();
+	const [kernelClient, setKernelClient] = useState<any>();
+	const [scaAddress, setScaAddress] = useState<any>();
+
+	useEffect(() => {
+		const fetchAccount = async () => {
+			const magicProvider = await magic?.wallet.getProvider();
+			const smartAccountSigner = await providerToSmartAccountSigner(
+				magicProvider
+			);
+
+			const client = await createEcdsaKernelAccountClient({
+				chain: polygonMumbai,
+				projectId: process.env.NEXT_PUBLIC_ZERODEV_SEPOLIA_PROJECT_ID!,
+				signer: smartAccountSigner,
+				paymaster: "NONE",
+			});
+			setKernelClient(client);
+
+			setScaAddress(client.account.address);
+		};
+
+		fetchAccount();
+	}, []);
+
+	return {
+		kernelClient,
+		scaAddress,
+	};
+};
